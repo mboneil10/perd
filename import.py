7812a83454631e7bcab22b8e1c24123952d032e4
Molly O'Neil <mbethoneil@gmail.com>
2020-07-25 17:49:06 -0400

    Specific attrs for the new website

--- import.py
+++ import.py
@@ -7,21 +7,21 @@ driver = webdriver.Chrome(ChromeDriverManager().install())
 
 page = "https://podcasts.apple.com/us/podcast/small-town-murder/id1194755213"
 
-title = []
-description = []
+episodes = []
+titles = []
+descriptions = []
 
 driver.get(page)
 
-# TODO: start with one episode and record attrs.
+# TODO: start with first few episodes and record attrs.
 content = driver.page_source
 data = BeautifulSoup(content, features = "html.parser")
 
-for row in data.findAll('tr', attrs={}):
-    keys = row.findAll('td', attrs={})
-    value = (keys[1]).text.strip()
-    person.append(value)
+for episode in data.findAll('li', attrs={'class':'ember-view tracks__track tracks__track--podcast'}):
+    title = episode.find('a', attrs={'class':'link tracks__track__link--block'}).text.strip()
+    titles.append(title)
 
 driver.close()
 
-df = pd.DataFrame({'Heading':person})
+df = pd.DataFrame({'Titles':titles})
 df.to_csv('products.csv', index=False, encoding='utf-8')
