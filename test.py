f4b3ef35a3e1f33c1821003bef3a2e2bffadf362
Molly O'Neil <mbethoneil@gmail.com>
2020-08-04 00:11:05 -0400

    One test passing three others should fail

--- test.py
+++ test.py
@@ -2,6 +2,22 @@ import pytest
 from data_import import location
 
 def test1():
-    starting_string = "Cruel, Unusual & Pointless in Newville, Pennsylvania"
+    # This one currently fails
+    starting_string = "Pointless in Newville, Pennsylvania"
     result_string = "Newville, Pennsylvania"
+    assert(location([starting_string]) == [result_string])
+
+def test2():
+    starting_string = "Tricky in American Fork, Utah"
+    result_string = "American Fork, Utah"
+    assert (location([starting_string]) == [result_string])
+
+def test3():
+    starting_string = "Bonus Episode!"
+    result_string = ""
+    assert (location([starting_string]) == [result_string])
+
+def test4():
+    starting_string = "in Antelope, California - Part: 1"
+    result_string = "in Antelope, California"
     assert(location([starting_string]) == [result_string])
